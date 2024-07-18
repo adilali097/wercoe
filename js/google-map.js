ad6f09c4033050e65c72a26fe7c79af30c43732d
Muhammed ALi <155841427+adilali097@users.noreply.github.com>
2024-07-18 20:36:41 +0530

    Update google-map.js

--- js/google-map.js
+++ js/google-map.js
@@ -1,22 +1,13 @@
-
 var google;
 
 function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
-    // 39.399872
-    // -8.224454
-    
+    // Latitude and longitude for the desired location
+    var myLatlng = new google.maps.LatLng(25.227683327691373, 55.17234742516456);
+
+    // Map options
     var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 7,
-
-        // The latitude and longitude to center the map (always required)
+        zoom: 15,
         center: myLatlng,
-
-        // How you would like to style the map. 
         scrollwheel: false,
         styles: [
             {
@@ -34,29 +25,19 @@ function init() {
         ]
     };
 
-    
-
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
+    // Get the HTML DOM element that will contain the map
     var mapElement = document.getElementById('map');
 
-    // Create the Google Map using out element and options defined above
+    // Create the Google Map using the element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
-    
-    var addresses = ['UAE'];
 
-    for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
-            new google.maps.Marker({
-                position: latlng,
-                map: map,
-                icon: 'images/loc.png'
-            });
-
-        });
-    }
-    
+    // Marker for the desired location
+    new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: 'Global best Accounting & Bookkeeping'
+    });
 }
+
+// Load the map after the page has finished loading
 google.maps.event.addDomListener(window, 'load', init);
